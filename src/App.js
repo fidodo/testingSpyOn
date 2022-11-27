0f38ed8cc84cfaae48e81108f6a7209db58e7db0
Ayokunle Ogunfidodo <ayokunleogunfidodo@gmail.com>
2022-11-27 23:32:50 +0200

    testing using spyOn

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [nationalities, setNationalities] = useState([]);
+  const [message, setMessage] = useState('');
+  const [personName, setPersonName] = useState('');
+
+  async function fetchNationalities() {
+    try {
+
+      const data = await (await fetch(`https://api.nationalize.io/?name=${personName}`)).json();
+      const hasCountryData = data.country && data.country.length
+      const nationalities =  hasCountryData ? data.country : [];
+      setNationalities(nationalities);
+
+      const message = hasCountryData ? `${data.country.length} guess(es) found` : 'No nationality match found';
+      setMessage(message);
+    } catch (err) {
+      console.log(`err: ${err.message}`);
+      setNationalities([]);
+      setMessage('Could not fetch nationalities, try again later.');
+    }
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    await fetchNationalities();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="title-form">
+          <h2>Check Name's Nationalities percent</h2>
+          <div style={{ marginBottom: '20px' }}>
+            <form name="nationalities-form" onSubmit={handleSubmit}>
+              <input
+                name="personName"
+                type="text"
+                onChange={(e) => setPersonName(e.target.value)}
+                value={personName}
+                placeholder="Enter a person's name"
+              />
+              <button onClick={handleSubmit}>Get Nationalities</button>
+            </form>
+          </div>
+        </div>
+        <div className="results">
+          <div className="message">{message}</div>
+          <div className="nationalities">
+            {Array.isArray(nationalities) && nationalities.map(
+              nationality => {
+                const flagUrl = `https://flagcdn.com/w160/${nationality.country_id.toLowerCase()}.jpg`;
+                const altText = `${nationality.country_id} flag`;
+                return <div key={nationality.country_id}><h3>{nationality.country_id} - {(nationality.probability * 100).toFixed(2)}%</h3> <img src={flagUrl} alt={altText} style={{
+                  border: "1px solid black"
+                }} /></div>
+              }
+            )}
+          </div>
+        </div>        
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,74 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen,fireEvent } from '@testing-library/react';
+import userEvent from "@testing-library/user-event"
+import mockFetch from "../mocks/mockFetch"
 import App from './App';
+
+let windowFetchSpy;
+
+beforeEach(() => {
+  windowFetchSpy = jest.spyOn(window, 'fetch').mockImplementation(mockFetch);
+})
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Check Name/i);
+  expect(titleElement).toBeInTheDocument();
+  expect(screen.getByRole('form')).toBeInTheDocument();
+  expect(screen.getByRole('textbox')).toBeInTheDocument();
+  expect(screen.getByRole('button', {name: 'Get Nationalities'})).toBeInTheDocument();
+});
+
+test('should get nationalities for a name', async() => {
+  render(<App />);
+
+  //simulate filling up the textbox
+  const personNameInput = screen.getByRole('textbox');
+  fireEvent.change(personNameInput, {target: {value: 'john'}})
+  expect(personNameInput.value).toBe('john');
+
+  //click the button
+   const getNationalitiesBtn = screen.getByRole('button', { name: 'Get Nationalities' });
+   expect(getNationalitiesBtn).not.toBeDisabled();
+   userEvent.click(getNationalitiesBtn);
+
+   //verify percent and flag images are displayed
+   expect(await screen.findByText('3 guess(es) found')).toBeVisible();
+
+   expect(windowFetchSpy).toHaveBeenCalled();
+   expect(windowFetchSpy).toHaveBeenCalledWith('https://api.nationalize.io/?name=john');
+   expect(screen.getByText('US - 4.84%')).toBeVisible();
+   expect(screen.getByText('IM - 4.44%')).toBeVisible();
+   expect(screen.getByText('IE - 4.21%')).toBeVisible();
+
+   const flagImages = screen.getAllByRole('img');
+   expect(flagImages).toHaveLength(3);
+   expect(flagImages[0]).toHaveAccessibleName('US flag');
+   expect(flagImages[1]).toHaveAccessibleName('IM flag');
+   expect(flagImages[2]).toHaveAccessibleName('IE flag');
+});
+
+test('should handle error while getting nationalities for a name', async() => {
+  //const consoleLogSpy = jest.spyOn(console, 'log').mockReturnValue(undefined);
+  const consoleLogSpy = jest.spyOn(console, 'log');
+
+   render(<App />);
+
+  const personNameInput = screen.getByRole('textbox');
+  fireEvent.change(personNameInput, {target: {value: 'error'}})
+  expect(personNameInput.value).toBe('error');
+
+  const getNationalitiesBtn = screen.getByRole('button', { name: 'Get Nationalities' });
+  expect(getNationalitiesBtn).not.toBeDisabled();
+  userEvent.click(getNationalitiesBtn);
+
+  expect(await screen.findByText('Could not fetch nationalities, try again later.')).toBeVisible();
+  expect(consoleLogSpy).toHaveBeenCalled();
+  expect(consoleLogSpy).toHaveBeenCalledWith(expect.stringContaining('err: Unhandled request'));
+
+  // const nationalities = container.getElementsByClassName('nationalities')[0];
+  // expect(nationalities).toBeEmptyDOMElement();
 });
